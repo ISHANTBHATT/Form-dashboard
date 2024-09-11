3e0c04b0771b14c14666e8fd821e6fe1c5c2503e
Ishant Bhatt <ishantbhatt1654@gmail.com>
2024-09-11 16:34:58 +0530

    changes: changes in footer

--- src/app/Components/Footer.jsx
+++ src/app/Components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <div className=" absolute right-8 bottom-8 ">
-      <p className="text-gray-400">
+      <p className="text-gray-400 text-[12px]">
         Application Developed by Apace Digital Cargo
       </p>
     </div>
